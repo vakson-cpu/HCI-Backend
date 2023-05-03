636239d5c98a82780f6e6e0f8ef0c5e20496e796
vakson-cpu <vakson12@gmail.com>
2023-05-03 03:25:56 +0200

    .

--- API/Conrollers/NBAController.js
+++ API/Conrollers/NBAController.js
@@ -91,16 +91,23 @@ const getStandingsByTeamId = async (req, res, next) => {
 };
 const getTeamById = async (req, res, next) => {
   const { id } = req.query;
-  let result = await axios
-    .get(`https://api-nba-v1.p.rapidapi.com/teams`, {
-      params: { id: `${id}` },
-      headers: {
-        "X-RapidAPI-Key": "3be10b1358msh51fd936d1571daep1230ccjsn529137f75def",
-        "X-RapidAPI-Host": "api-nba-v1.p.rapidapi.com",
-      },
-    })
-    .then((res) => res.data.response)
-    .catch((err) => err);
+  let result;
+  try {
+    result = await axios
+      .get(`https://api-nba-v1.p.rapidapi.com/teams`, {
+        params: { id: `${id}` },
+        headers: {
+          "X-RapidAPI-Key":
+            "3be10b1358msh51fd936d1571daep1230ccjsn529137f75def",
+          "X-RapidAPI-Host": "api-nba-v1.p.rapidapi.com",
+        },
+      })
+      .then((res) => res.data.response)
+      .catch((err) => err);
+  } catch (err) {
+    const error = new HttpError("Something went wrong", 500, false);
+    next(error);
+  }
   // const colorThief = new ColorThief();
 
   const color = await ColorThief.getColor(result[0].logo); //Ovde kupi sa linka
@@ -154,8 +161,15 @@ const getPlayerStatistics = async (req, res, next) => {
   // let points=result.sort((a, b) => b.points - a.points).slice(0,3);
   // let assists=result.sort((a,b)=>b.assists-a.assists).slice(0,3);
   // let rebounds=result.sort((a,b)=>b.totReb-a.totReb).slice(0,3);
-  let filteredList = result.filter(item=>item.min>0).sort((a, b) => (b.points+b.assists+b.toReb) - (a.points+a.assists+a.toReb));
-  return res.status(200).json({players:filteredList,number:filteredList.length});
+  let filteredList = result
+    .filter((item) => item.min > 0)
+    .sort(
+      (a, b) =>
+        b.points + b.assists + b.toReb - (a.points + a.assists + a.toReb)
+    );
+  return res
+    .status(200)
+    .json({ players: filteredList, number: filteredList.length });
 };
 exports.getGames = getGames;
 exports.getSeasons = getSeasons;
