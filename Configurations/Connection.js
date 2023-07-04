e41f5fa9d3e0f1db3817fd1c06fdde3c16ac40f5
Vahid Uglic <73440922+vakson-cpu@users.noreply.github.com>
2023-07-04 04:58:41 +0200

    Update Connection.js

--- Configurations/Connection.js
+++ Configurations/Connection.js
@@ -6,7 +6,7 @@ const SeedingRoles = require("../Data/SeedRoles")
 const ConnectToDatabase=async(app,PORT)=>{
 return await mongoose
   .connect(
-    `mongodb+srv://vakson12:omikron12345@hci.fmslwy1.mongodb.net/HCI?retryWrites=true&w=majority`
+    `mongodb+srv://vakson12://@hci.fmslwy1.mongodb.net/HCI?retryWrites=true&w=majority`
   )
   .then(() => {
     app.listen(PORT);
